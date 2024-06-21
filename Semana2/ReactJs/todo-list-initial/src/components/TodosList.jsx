97f6b2d9cfa54764f39af0c23a4150cf763d5607
glpontes <gabriel.pontes@dcx.ufpb.br>
2024-06-21 14:10:18 -0300

    context api e reducers

---
+++ Semana2/ReactJs/todo-list-initial/src/components/TodosList.jsx
@@ -0,0 +1,27 @@
+import {useTodos} from '../TodosContext.jsx'
+import Todo from './Todo.jsx';
+
+
+
+function TodosList() {
+
+  const store = useTodos();
+
+  return (
+    <>
+        <div className="todos">
+
+            {store.filteredTodos().length ? store.filteredTodos().map(todo => 
+              <Todo
+                todo ={todo}
+                key={todo.id}
+              />
+            ) : 'No to-dos to be shown. Try clearing the filter or add a new task'
+          }
+
+        </div>
+    </>
+  )
+}
+
+export default TodosList
