9c720e47135d0394da062926e0993e87975b50ea
glpontes <gabriel.pontes@dcx.ufpb.br>
2024-06-28 13:42:27 -0300

    concluido curso de spring

--- Semana2/SpringBoot/sb-project/src/main/java/sinerji/glpontes/config/SecurityConfig.java
+++ Semana2/SpringBoot/sb-project/src/main/java/sinerji/glpontes/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UsuarioServiceImpl usuarioService;
 
     @Autowired
-    private JwtService jwtService
+    private JwtService jwtService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -63,6 +64,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "v2/api-docs",
+                "configuration/ui",
+                "swagger-resources/**",
+                "configuration/security",
+                "/swagger-ui.html",
+                "webjars/**");
     }
 
 }
--- Semana2/SpringBoot/sb-project/src/main/java/sinerji/glpontes/domain/entity/Usuario.java
+++ Semana2/SpringBoot/sb-project/src/main/java/sinerji/glpontes/domain/entity/Usuario.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column
